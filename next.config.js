f9ba8ac8c442ef33ddbc24ad8e90670182e73335
Diego Medina <diegomedina@Diegos-Air-3.attlocal.net>
2022-11-05 01:35:34 -0400

    Working db call and janky usage

--- next.config.js
+++ next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false
+      config.resolve.fallback.tls = false
+      config.resolve.fallback.net = false
+      config.resolve.fallback.child_process = false
+    }
+
+    return config
+  },
+  env: {
+    NEXT_PUBLIC_GOOGLE_SHEET_ID: process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID,
+    NEXT_PUBLIC_GOOGLE_SHEET_CLIENT_EMAIL: process.env.NEXT_PUBLIC_GOOGLE_SHEET_CLIENT_EMAIL,
+    NEXT_PUBLIC_GOOGLE_SHEET_PRIVATE_KEY: process.env.NEXT_PUBLIC_GOOGLE_SHEET_PRIVATE_KEY,
+    BASE_URL: process.env.BASE_URL,
+  }
 }
 
 module.exports = nextConfig
--- pages/_map.js
+++ pages/_map.js
@@ -1,15 +1,83 @@
-import React from 'react';
-import styles from '../styles/Home.module.css'
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import L from "leaflet";
+import { GoogleSpreadsheet } from "google-spreadsheet"
+
 import 'leaflet/dist/leaflet.css'
 import 'leaflet/dist/leaflet.js'
-
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import styles from '../styles/Home.module.css'
 
 const position = [39.8283, -98.5795]
 const icon = L.icon({ iconUrl: "/flags/nicaragua.png", className: styles.marker });
+const doc = new GoogleSpreadsheet(process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID)
 
 export default function Map() {
+  const [rows, setRows] = useState([])
+  const [markers, setMarkers] = useState([])
+
+  useEffect(() => {
+    async function getData(doc) {
+      await doc.useServiceAccountAuth({
+        client_email: process.env.NEXT_PUBLIC_GOOGLE_SHEET_CLIENT_EMAIL,
+        private_key: process.env.NEXT_PUBLIC_GOOGLE_SHEET_PRIVATE_KEY
+      }, function(err) {
+        if(!err){
+          doc.getRow(1,function(error,row){
+              if(!error && row){
+                console.log(row);
+              }else{
+                 console.log('ERROR IN getRow :'+error);
+              }
+           });
+
+        }else{
+          console.log('ERROR IN useServiceAccountAuth : '+err);
+        }
+      });
+      await doc.loadInfo()
+      const sheet = doc.sheetsByIndex[0]
+      const rows = await sheet.getRows();
+      setRows(rows)
+    }
+    getData(doc)
+  }, [])
+
+  useEffect(()=> {
+    rows.filter((row)=> {
+      const name = row['Full name']
+      const zipcode = row['Zipcode']
+      const country = row['Country']
+
+      if(country && zipcode && name) {
+        return true
+      } else {
+        return false
+      }
+    }).map((row)=> {
+      const name = row['Full name']
+      const zipcode = row['Zipcode']
+      const country = row['Country']
+      let flagURL = ''
+      const str = country.replace(/\s+/g, '-').toLowerCase()
+      flagURL = `/flags/${str}.png`
+  
+      const marker = {
+        message: `This is where ${name} lives. <br /> Their family is from ${country}.`,
+        position: [Math.random()/5*100+30, -Math.random()/2*100-70],
+        icon: L.icon({ iconUrl: flagURL, className: styles.marker })
+      }
+      setMarkers(markers => [...markers, marker]);
+    })
+  }, [rows])
+
+  const markerComponent = ((markerObj) => (
+    <Marker position={markerObj.position} icon={markerObj.icon}>
+      <Popup>
+        {markerObj.message}
+      </Popup>
+    </Marker>
+  ))
+
   return (
     <div>
       <MapContainer center={position} zoom={4} scrollWheelZoom={false} style={{height: '600px',width: '90vw'}}>
@@ -17,17 +85,9 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position} icon={icon}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-
-        <Marker position={[52.515, -0.08]}>
-          <Popup>
-            Test.
-          </Popup>
-        </Marker>
+        {markers.map((marker)=> (
+          markerComponent(marker)
+        ))}
       </MapContainer>
     </div>
     
